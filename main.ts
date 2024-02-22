0f3a75e79553249e1e81a20dc9d1d9c51d9b9d84
nono38 <74985866+nono38@users.noreply.github.com>
2024-02-22 13:15:36 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+pins.servoWritePin(AnalogPin.P0, 0)
+basic.pause(1000)
+pins.servoWritePin(AnalogPin.P0, 90)
+basic.pause(1000)
+pins.servoWritePin(AnalogPin.P0, 180)
+basic.pause(1000)
+pins.servoWritePin(AnalogPin.P0, 270)
+basic.pause(1000)
+basic.forever(function () {
+	
+})
